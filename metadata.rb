3c0540ed3183730d815df4c267911fcc20a1c3e9
Joaquim Dos Santos <jdossantos@ikoula.com>
2014-06-27 12:57:22 +0200

    Include name for Ridley and debian os support on metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name              "cloudera"
 maintainer        "Daniel P."
 description       "Installs Cloudera for Ubuntu only"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -6,6 +7,6 @@ version           "1.5.5"
 recipe "cloudera", "Installs Cloudera Pseudo (all on one node)"
 recipe "cloudera::flume", "Installs Apache Flume"
 
-%w{ ubuntu }.each do |os|
+%w{ ubuntu debian }.each do |os|
   supports os
 end
